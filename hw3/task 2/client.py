1b09c9f47b9ca439fe46327662ba9e6060ffce7d
Alex Silyuk <alex.silyuk@gmail.com>
2018-12-15 15:59:53 +0200

    done task 2,  lost message now recovered well
    screenshots added

--- hw3/task 2/client.py
+++ hw3/task 2/client.py
@@ -14,15 +14,11 @@ udp_socket = socket(AF_INET, SOCK_DGRAM)
 packetNumber=0
 
 # create list of words for message
-message = "He was so proud of his little girl.  It was her very first day of school.  He walked with her to school that day,  And she held his hand all the way.  They walked together quiet and sad,  A little girl and her loving dad.  Into the school her father led,  But he almost cried when she said,  \"Daddy, Daddy, please don't go.  Don't leave me here all alone.  I'll miss you if you go away,  And I might need you; can't you stay?\"  \"Little Daughter, please don't cry.  You'll be okay, so dry your eyes.  You have our memories in your heart.  We're together though we're apart.\"    He sat up front on her wedding day  And cried as his daughter walked away.  Later that night he watched her dance.  He sat there waiting for his chance.  The band started to play their song.  Father and daughter danced along.  She looked at him and saw a tear  Then leaned and whispered in his ear,  \"Daddy, Daddy, I have to go.  I hate to leave you all alone.  I'll miss you when I go away,  But if you need me then I'll stay.\"  \"Little Daughter, I'll be just fine.  I'll love you always; you are mine.  I have our memories in my heart.  We're together though we're apart.\"    She came in his room and kissed his head  Then sat next to his hospital bed.  He took her hand and held it tight  And wished he had the strength to fight.  They sat together, quiet and sad,  A daughter and her dying dad.  He saw the tears she tried to hide.  She looked at him and then she cried.  \"Daddy, Daddy, please don't go.  Don't leave me here all alone.  I'll miss you when you go away.  I still need you; you have to stay.\"  \"Little Daughter, I love you so.  I want to stay but have to go.  I'll always be here in your heart.  We're together though we're apart.\"".split(" ")
+message = "He was so proud of his little girl.  It was her very first day of school.  He walked with her to school that day,  And she held his hand all the way.  They walked together quiet and sad,  A little girl and her loving dad.  Into the school her father led,  But he almost cried when she said,  Daddy, Daddy, please dont go.  Dont leave me here all alone.  I will miss you if you go away,  And I might need you; cant you stay?  Little Daughter, please don't cry.  You'll be okay, so dry your eyes.  You have our memories in your heart.  We're together though we're apart. He sat up front on her wedding day  And cried as his daughter walked away.  Later that night he watched her dance.  He sat there waiting for his chance.  The band started to play their song.  Father and daughter danced along.  She looked at him and saw a tear  Then leaned and whispered in his ear,  Daddy, Daddy, I have to go.  I hate to leave you all alone.  I'll miss you when I go away,  But if you need me then I'll stay. Little Daughter, I'll be just fine.  I'll love you always; you are mine.  I have our memories in my heart.  We're together though we're apart.  She came in his room and kissed his head  Then sat next to his hospital bed.  He took her hand and held it tight  And wished he had the strength to fight.  They sat together, quiet and sad,  A daughter and her dying dad.  He saw the tears she tried to hide.  She looked at him and then she cried.  Daddy, Daddy, please don't go.  Don't leave me here all alone.  I'll miss you when you go away.  I still need you; you have to stay. Little Daughter, I love you so.  I want to stay but have to go.  I'll always be here in your heart.  We're together though we're apart".split(" ")
 
-#makes xor between 2 strings
-def xor(s1,s2):  
-	if(s1 == 0):
-		return s2
-	if(s2 == 0):
-		return s1
-	return ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(s1,s2))
+
+# makes xor between 2 strings, cc*100 adding zeroes in case of short string
+xor = lambda ss,cc: ''.join(chr(ord(s)^ord(c)) for s,c in zip(ss,cc*100))
 
 
 # generate message for sending
@@ -33,50 +29,61 @@ def generateMessage():
 
 	for _ in range(randrange(1,len(message))):
 		random_index = randrange(0,len(message))
-		data = message[random_index]+" "+data
+		data = data + " "+message[random_index]
 
 	return data
 
-
+# send udp datagram and not waiting to response
 def sendPacket(data):
 	udp_socket.sendto(data, addr)
 	data = bytes.decode(data)
 	
+# send packet with d value
 def sendDPacket(dValue):
 	sendPacket("d="+str(dValue))	#send d value to server
 	responce = udp_socket.recvfrom(1024)
-	print responce[0]
+	print responce[0]	# and wait for responce, kind of negotiation
 
-def sendMessage(data):
-	totalpackets=(len(data)/100 )+1
-	d=randrange(1,totalpackets)
-	sendDPacket(d)
-
-	dataBackup=data
-
-	e=0
-	for _ in range(totalpackets):
+# xoring all datagrams
+def calculateEvalue(totalpackets,data):
+	e=data[:100]
+	data=data[100:]
+	for _ in range(totalpackets-1):
 		e=xor(e,data[:100])
-		data=data=data[100:]
+		data=data[100:]
 
-	data=dataBackup
+	return e
+
+# split message to datagrams, sending d value, sending all packets
+def sendMessage(data):
+	totalpackets=(len(data)/100 )+1	# calculate total amount of packets
+	d=randrange(1,totalpackets)		# generate d value
+	sendDPacket(d)					# send d value to server
+
+	e=calculateEvalue(totalpackets,data)
+
+	# split message to datagrams, to firs d packets, append to tail "E=" with calculated e value
 	for packetID in range(totalpackets):
-		header=str(packetID+1)+"/"+str(totalpackets)+"|"
+		header=str(packetID+1)+"/"+str(totalpackets)+"|"	# header separated from message with "|" symbol
 		packetData=data[:100]
 		if packetID < d:
-			packet=header+packetData+"E="+e
+			packet=header+packetData+"||E="+e
 		else:
 			packet=header+packetData
 		
 		data=data[100:]
+
+		# simulates packet lost, not sending 3-rd packet (indices are: 0,1,2..)
 		if packetID ==2:
 			print "lost Packet: "+packetData
 			continue
+
+		#send generated packet to server
 		sendPacket(packet)
 
 	return totalpackets
 
-
+# receive echo from server
 def receiveAnswer(packetsAmount):
 	print "Starting to echo from server..."
 	print "\n**************************  PACKET [ ",packetNumber," ]  **************************\n"
@@ -87,23 +94,27 @@ def receiveAnswer(packetsAmount):
 	print "\n**************************  PACKET [ ",packetNumber," ] END  ***********************\n"
 
 
+# generate packet, sends it and receive answers
 def generatePacket():
-	data = generateMessage()
 	global packetNumber
 	packetNumber+=1
 	packetsAmount=1
+
+	# generate random message
+	data = generateMessage()
+	# split message to datagrams and send them to server
 	packetsAmount = sendMessage(data)
 
 	print "Packet #"+str(packetNumber)+", splitted to "+str(packetsAmount)+" datagrams was sent"
+
+	# wait for answer from server to echo packets
 	receiveAnswer(packetsAmount)
 
 
 
+#send packet each 3 second
+while True:
+	generatePacket()
+	time.sleep(3)
 
-# while True:
-# 	generatePacket()
-# 	time.sleep(3)
-
-
-generatePacket()
 udp_socket.close()
--- hw3/task 2/server.py
+++ hw3/task 2/server.py
@@ -1,101 +1,128 @@
 from socket import *
 
+### socket config ###
 host = '192.168.42.140'
 port = 12321
 addr = (host,port)
 udp_socket = socket(AF_INET, SOCK_DGRAM)
 udp_socket.bind(addr)
+#####################
 
+### global variables ###
 currentPacketNum=0
 d=0
 e=0
 packetBuffer=[]
+########################
 
-#makes xor between 2 strings
-def xor(s1,s2):  
-	if(s1 == 0):
-		return s2
-	if(s2 == 0):
-		return s1
-	return ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(s1,s2))
+# makes xor between 2 strings
+xor = lambda ss,cc: ''.join(chr(ord(s)^ord(c)) for s,c in zip(ss,cc*100))
 
+
+# parse d value from packet
 def receiveDvalue(message):
+	global d
+	#check if d packet arrived
 	if message[:2] == "d=":
 		print "received d="+str(message[2:])
-		d=int(message[2:])
+		d=int(message[2:]) # cut d value from message
 		answer="reveived d="+str(d)+" by server"
-		udp_socket.sendto(answer,addr)
+		udp_socket.sendto(answer,addr) # send message to client to know taht we got d value
 
+		# renew packets counter
 		global currentPacketNum
 		currentPacketNum=0
-		return d
 
+# splite header of type 1/12, while 1 is curent packet id and 12 is total packets
 def parseHeader(header):
 	return header.split('/',1)
 
+# parse received message
 def parseMessage(message):
 	global currentPacketNum
 	currentPacketNum+=1
 	
+	# header separated from data with "|" sign
 	parsedMessage=message.split("|",1)
 	header=parsedMessage[0]
-	parsedMessage=parsedMessage[1].rsplit("E=",1)
+
+	# detect if packet contains E value, in right of datagram message
+	parsedMessage=parsedMessage[1].split("||E=",1)
 	if len(parsedMessage)>1:
 		global e
 		e=parsedMessage[1]
 
+	# get current packet id and total of packets
 	currentID,totalPackets=parseHeader(header)
-	
-	print "packet "+str(currentID)+ " of "+ str(totalPackets)+" received"
-
-	sequencNumber=currentID
 
 	#detect packet lost
-	if int(sequencNumber) != int(currentPacketNum):
-		packetBuffer.append("None")
+	if int(currentID) != int(currentPacketNum):
+		packetBuffer.append("None") # add "None" instead of lost packet for future recovering
 		currentPacketNum+=1
 
-		packetBuffer.append(parsedMessage[0])
-	else:
-		packetBuffer.append(parsedMessage[0])
+	# add received message to buffer
+	packetBuffer.append(parsedMessage[0])
 
+	print "packet "+str(currentID)+ "/"+ str(totalPackets)+" received"
+
+	# detect if it's last datagram in sequence is arrived, to know when to start echo
 	if int(currentID) == int(totalPackets):
 		return True
 	else:
 		return False 
-	
-def sendResponce():
-	global packetBuffer
-	calculated_e=0
 
-	# calculate e of all received messages
-	for line in packetBuffer:
+# calculate e value based on all correct received messages
+def calculateEvalue():
+	calculated_e=packetBuffer[0]
+	for i,line in enumerate(packetBuffer):
+		if i==0:				# skip first message, beacuse we took it already 
+			continue
+
+		# with "None" we signed in buffer place of lost packet
+		# if current value not "None", xor it 
 		if line != "None":
 			calculated_e=xor(calculated_e,line)
+
+	return calculated_e
+
+
+# after receiving all messages, send received packets back
+def sendResponce():
+	global packetBuffer
+
+	# calculate e value of all received datagrams
+	calculated_e=calculateEvalue()
 
 	# find index of lost packet in buffer and calculate it's value
 	# it's can recover only one one lost datagram
 	for index, line in enumerate(packetBuffer):
 		if line == "None":
 			packetBuffer[index]=str(xor(calculated_e,e))
-			print "Lost packet recovered: "+packetBuffer[index]
+			print "Recovered packet:\nID: "+str(index+1)+", Data: "+packetBuffer[index]
+			break
 
-
+	# send all packets back to client
 	print "Starting to send responce"
 	for line in packetBuffer:
-		print "send: "+line
 		udp_socket.sendto(line,addr)
-	packetBuffer=[]
+
+	del packetBuffer[:]
 
 while True:
-	message, addr = udp_socket.recvfrom(10)
-	d=receiveDvalue(message)
+	#receive first packet with "d" value
+	message, addr = udp_socket.recvfrom(1000)
+	print "***************** Receiving packet *****************"
+	receiveDvalue(message)
 
+	# receive packets till last datagram arrived
 	while True:
 		message, addr = udp_socket.recvfrom(1000)
 		isLastMessage=parseMessage(message)
 		if isLastMessage is True:
+			# after receiving all datagrams, send then back to client
 			sendResponce()
 			break
+	print "***************** Packet received  *****************"
 
+# close connection
 udp_socket.close()
